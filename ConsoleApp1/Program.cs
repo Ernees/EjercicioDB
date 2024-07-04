8dc2508109ca988ee4a3ac0bd01d8d5fca380e0c
Ernees <huertaernesto04@gmail.com>
2024-07-04 18:51:07 -0300

    DataBase de Empleado

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Clases;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatoEmpleado acceso = new DatoEmpleado("localhost", "212_organizacion");
+
+            //Empleado nuevoEmpleado = new Empleado();
+            //nuevoEmpleado.Id = 6; 
+            //nuevoEmpleado.Nombre = "Ernesto";
+            //nuevoEmpleado.Apellido = "Huerta";
+            //nuevoEmpleado.Sueldo = 56123423;
+
+            acceso.EliminarEmpleado(20);
+
+            //List<Empleado> miLista;
+
+            //miLista = acceso.SeleccionarEmpleados();
+            //foreach (Empleado mi in miLista)
+            //{
+            //    Console.WriteLine(mi.ToString());
+            //}
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
